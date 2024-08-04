c57da9836c857158f16283a34f02a87cbd45a92b
admiralhr99 <admiralhr99@gmail.com>
2024-08-04 16:15:34 +0330

    Changes added

--- go.mod
+++ go.mod
@@ -4,8 +4,9 @@ go 1.21.3
 
 require (
 	github.com/google/go-github/v38 v38.1.0 // indirect
-	github.com/google/go-querystring v1.0.0 // indirect
-	golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2 // indirect
+	github.com/google/go-github/v39 v39.2.0 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
 	golang.org/x/oauth2 v0.21.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,122 +2,100 @@ package main
 
 import (
 	"context"
-	"crypto/tls"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io"
-	"log"
 	"net/http"
-	"net/url"
 	"os"
-	"path/filepath"
 	"regexp"
 	"strings"
 	"time"
 
-	"github.com/google/go-github/v38/github"
-	"golang.org/x/oauth2"
+	"github.com/google/go-github/v39/github"
 )
 
-const (
-	owner = "projectdiscovery"
-	repo  = "nuclei-templates"
-)
-
-var (
-	silent     bool
-	logger     *log.Logger
-	httpClient *http.Client
-)
+type PullRequest struct {
+	Title     string    `json:"title"`
+	CreatedAt time.Time `json:"created_at"`
+	HTMLURL   string    `json:"html_url"`
+	User      struct {
+		Login string `json:"login"`
+	} `json:"user"`
+	Head struct {
+		SHA string `json:"sha"`
+	} `json:"head"`
+}
 
 func main() {
-	var downloadDir string
-	var proxyURL string
-	flag.StringVar(&downloadDir, "dir", ".", "Directory to download YAML files (default: current directory)")
-	flag.StringVar(&proxyURL, "proxy", "", "HTTP proxy URL (optional)")
-	flag.BoolVar(&silent, "silent", false, "Silent mode: don't print anything")
+	owner := "projectdiscovery"
+	repo := "nuclei-templates"
+
+	outputFile := flag.String("output", "pull_requests.json", "Output file for pull requests")
+	silent := flag.Bool("silent", false, "Silent mode")
+	download := flag.Bool("download", false, "Download YAML files")
 	flag.Parse()
 
-	if silent {
-		logger = log.New(io.Discard, "", 0)
-	} else {
-		logger = log.New(os.Stdout, "", log.LstdFlags)
-	}
-
-	token := os.Getenv("GITHUB_TOKEN")
-	if token == "" {
-		logger.Fatal("GITHUB_TOKEN environment variable not set")
-	}
-
-	// Set up HTTP client with proxy and TLS config
-	httpClient = &http.Client{
-		Transport: &http.Transport{
-			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-		},
-	}
-
-	if proxyURL != "" {
-		proxyURLParsed, err := url.Parse(proxyURL)
-		if err != nil {
-			logger.Fatalf("Invalid proxy URL: %v", err)
-		}
-		httpClient.Transport.(*http.Transport).Proxy = http.ProxyURL(proxyURLParsed)
-	}
-
+	client := github.NewClient(nil)
 	ctx := context.Background()
-	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: token})
-	tc := oauth2.NewClient(ctx, ts)
-	tc.Transport = httpClient.Transport
-	client := github.NewClient(tc)
 
-	newCVEs, err := fetchNewCVEs(ctx, client)
-	if err != nil {
-		logger.Fatalf("Error fetching new CVEs: %v", err)
+	oneMonthAgo := time.Now().AddDate(0, -1, 0)
+
+	if err := fetchPullRequests(ctx, client, owner, repo, oneMonthAgo, *outputFile, *silent); err != nil {
+		fmt.Fprintf(os.Stderr, "Error fetching pull requests: %v\n", err)
+		os.Exit(1)
 	}
 
-	downloadedCount := 0
-	for _, cve := range newCVEs {
-		err = downloadYAMLFiles(ctx, client, cve, downloadDir)
-		if err != nil {
-			logger.Printf("Error downloading YAML files for %s: %v", cve, err)
-		} else {
-			downloadedCount++
+	if *download {
+		if err := downloadYAMLFiles(*outputFile); err != nil {
+			fmt.Fprintf(os.Stderr, "Error downloading YAML files: %v\n", err)
+			os.Exit(1)
 		}
-	}
-
-	if !silent {
-		logger.Printf("Finished processing. Downloaded YAML files for %d new CVEs.", downloadedCount)
 	}
 }
 
-func fetchNewCVEs(ctx context.Context, client *github.Client) ([]string, error) {
-	var newCVEs []string
-	oneMonthAgo := time.Now().AddDate(0, -1, 0)
-
-	opt := &github.PullRequestListOptions{
-		State:     "all",
-		Sort:      "updated",
+func fetchPullRequests(ctx context.Context, client *github.Client, owner, repo string, since time.Time, outputFile string, silent bool) error {
+	opts := &github.PullRequestListOptions{
+		State:     "open",
+		Sort:      "created",
 		Direction: "desc",
 		ListOptions: github.ListOptions{
 			PerPage: 100,
 		},
 	}
 
+	var allPRs []PullRequest
+
 	for {
-		prs, resp, err := client.PullRequests.List(ctx, owner, repo, opt)
+		prs, resp, err := client.PullRequests.List(ctx, owner, repo, opts)
 		if err != nil {
-			return nil, err
+			return err
 		}
 
 		for _, pr := range prs {
-			if pr.GetUpdatedAt().Before(oneMonthAgo) {
-				return newCVEs, nil
+			if pr.CreatedAt.Before(since) {
+				break
 			}
 
-			if strings.Contains(strings.ToLower(pr.GetTitle()), "cve") {
-				cve := extractCVE(pr.GetTitle())
-				if cve != "" {
-					newCVEs = append(newCVEs, cve)
+			if strings.Contains(strings.ToLower(*pr.Title), "cve") {
+				allPRs = append(allPRs, PullRequest{
+					Title:     *pr.Title,
+					CreatedAt: *pr.CreatedAt,
+					HTMLURL:   *pr.HTMLURL,
+					User: struct {
+						Login string `json:"login"`
+					}{
+						Login: *pr.User.Login,
+					},
+					Head: struct {
+						SHA string `json:"sha"`
+					}{
+						SHA: *pr.Head.SHA,
+					},
+				})
+
+				if !silent {
+					fmt.Printf("New PR: %s\n", *pr.Title)
 				}
 			}
 		}
@@ -125,55 +103,140 @@ func fetchNewCVEs(ctx context.Context, client *github.Client) ([]string, error) 
 		if resp.NextPage == 0 {
 			break
 		}
-		opt.Page = resp.NextPage
+		opts.Page = resp.NextPage
 	}
 
-	return newCVEs, nil
+	return writeToFile(allPRs, outputFile)
 }
 
-func extractCVE(title string) string {
-	re := regexp.MustCompile(`CVE-\d{4}-\d+`)
-	match := re.FindString(title)
-	return match
+func writeToFile(prs []PullRequest, filename string) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "  ")
+	return encoder.Encode(prs)
 }
 
-func downloadYAMLFiles(ctx context.Context, client *github.Client, cve, downloadDir string) error {
-	opt := &github.SearchOptions{
-		ListOptions: github.ListOptions{PerPage: 100},
+func downloadYAMLFiles(inputFile string) error {
+	file, err := os.Open(inputFile)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	var prs []PullRequest
+	if err := json.NewDecoder(file).Decode(&prs); err != nil {
+		return err
 	}
 
-	query := fmt.Sprintf("repo:%s/%s filename:%s.yaml path:cves", owner, repo, cve)
-	for {
-		result, resp, err := client.Search.Code(ctx, query, opt)
-		if err != nil {
-			return err
+	paths := []string{
+		"http/cves",
+		"network/cves",
+		"passive/cves",
+		"code/cves",
+		"headless/cves",
+		"dast/cves",
+		"javascript/cves",
+	}
+
+	for _, pr := range prs {
+		cve := extractCVE(pr.Title)
+		if cve == "" {
+			fmt.Printf("Skipping PR: %s (No CVE found in title)\n", pr.Title)
+			continue
 		}
 
-		for _, file := range result.CodeResults {
-			downloadURL := file.GetHTMLURL()
-			downloadURL = strings.Replace(downloadURL, "github.com", "raw.githubusercontent.com", 1)
-			downloadURL = strings.Replace(downloadURL, "/blob/", "/", 1)
+		year := extractYear(cve)
+		if year == "" {
+			fmt.Printf("Skipping PR: %s (No year found in CVE)\n", pr.Title)
+			continue
+		}
 
-			localPath := filepath.Join(downloadDir, filepath.Base(file.GetName()))
-			err := downloadFile(downloadURL, localPath)
-			if err != nil {
-				logger.Printf("Error downloading file %s: %v", file.GetName(), err)
-			} else if !silent {
-				logger.Printf("Successfully downloaded: %s", localPath)
+		cveUpper := strings.ToUpper(strings.TrimSuffix(cve, ".yaml"))
+		filename := cveUpper + ".yaml"
+		lowercaseFilename := strings.ToLower(cveUpper) + ".yaml"
+
+		var downloadedURL string
+		for _, path := range paths {
+			url := fmt.Sprintf("https://raw.githubusercontent.com/projectdiscovery/nuclei-templates/%s/%s/%s/%s",
+				pr.Head.SHA,
+				path,
+				year,
+				filename)
+
+			if err := downloadFile(url, filename); err == nil {
+				downloadedURL = url
+				break
 			}
 		}
 
-		if resp.NextPage == 0 {
-			break
+		// If standard paths fail, try root with uppercase filename
+		if downloadedURL == "" {
+			url := fmt.Sprintf("https://raw.githubusercontent.com/projectdiscovery/nuclei-templates/%s/%s",
+				pr.Head.SHA,
+				filename)
+
+			if err := downloadFile(url, filename); err == nil {
+				downloadedURL = url
+			}
 		}
-		opt.Page = resp.NextPage
+
+		// If uppercase in root fails, try lowercase in root
+		if downloadedURL == "" {
+			url := fmt.Sprintf("https://raw.githubusercontent.com/projectdiscovery/nuclei-templates/%s/%s",
+				pr.Head.SHA,
+				lowercaseFilename)
+
+			if err := downloadFile(url, filename); err == nil {
+				downloadedURL = url
+			}
+		}
+
+		if downloadedURL != "" {
+			fmt.Printf("Downloaded: %s\n", downloadedURL)
+		} else {
+			fmt.Printf("Failed to download %s from any path\n", filename)
+		}
 	}
 
 	return nil
 }
 
-func downloadFile(url, filePath string) error {
-	resp, err := httpClient.Get(url)
+func extractCVE(title string) string {
+	// First, try to find CVE pattern in the whole string
+	re := regexp.MustCompile(`(?i)CVE-\d{4}-\d{4,7}`)
+	match := re.FindString(title)
+	if match != "" {
+		return strings.TrimSuffix(match, ".yaml")
+	}
+
+	// If no match found, split by common delimiters and check each part
+	parts := strings.FieldsFunc(title, func(r rune) bool {
+		return r == ' ' || r == ',' || r == ';' || r == ':'
+	})
+	//parts := strings.Fields(title)
+	for _, part := range parts {
+		if strings.HasPrefix(strings.ToUpper(part), "CVE-") {
+			return strings.TrimSuffix(part, ".yaml")
+		}
+	}
+	return ""
+}
+
+func extractYear(cve string) string {
+	parts := strings.Split(cve, "-")
+	if len(parts) >= 2 {
+		return parts[1]
+	}
+	return ""
+}
+
+func downloadFile(url, filename string) error {
+	resp, err := http.Get(url)
 	if err != nil {
 		return err
 	}
@@ -183,7 +246,7 @@ func downloadFile(url, filePath string) error {
 		return fmt.Errorf("bad status: %s", resp.Status)
 	}
 
-	out, err := os.Create(filePath)
+	out, err := os.Create(filename)
 	if err != nil {
 		return err
 	}
